4b71e0a7547669d90e6c0f7af41f62057f1b54f6
jureamer <wngud4950@gmail.com>
2024-09-01 21:04:45 +0900

    initialize

---
+++ src/main/java/com/example/mknewsscrappingbot/MkNewsScrappingBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.mknewsscrappingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkNewsScrappingBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkNewsScrappingBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mknewsscrappingbot/MkNewsScrappingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mknewsscrappingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkNewsScrappingBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
